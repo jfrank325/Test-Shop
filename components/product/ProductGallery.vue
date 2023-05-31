e14443b812d5cbdfaf330f0891701f3dc6603e9b
jfrank325 <jfrank325@gmail.com>
2023-05-31 18:39:41 +0200

    Basic Structure

---
+++ components/product/ProductGallery.vue
@@ -0,0 +1,16 @@
+<script setup lang='ts'>
+import Product from '~/types/product'
+const props = defineProps<{ products: Product[] }>();
+</script>
+
+<template>
+    <div>
+        <ul class="flex flex-col md:flex-row">
+            <li class="w-fit mx-auto" v-for="product of products">
+                <ProductCard :product="product"></ProductCard>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<style scoped></style>
